bbddd66190f68d3a8e745efd9f9b68cb6ff2a875
MODriscoll <MODriscoll55@outlook.com>
2019-05-31 21:27:51 +1000

    Brainstormed how to work the Wires and WireManager

---
+++ Assets/Scripts/Wires/SparkJumper.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Liminal.Wires
+{
+    /// <summary>
+    /// Base for the player controller for the wires game mode
+    /// </summary>
+    public class SparkJumper : MonoBehaviour
+    {
+        protected Spark m_Spark;
+        [SerializeField] private LayerMask m_SparkLayers;
+
+        /// <summary>
+        /// Traces the world for other sparks, jumping to them if allowed
+        /// </summary>
+        /// <param name="origin">Origin of the trace</param>
+        /// <param name="direction">Direction of the trace</param>
+        protected void TraceSpark(Vector3 origin, Quaternion direction)
+        {
+            Ray ray = new Ray(origin, direction * Vector3.forward);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, m_SparkLayers, QueryTriggerInteraction.Ignore))
+            {
+                GameObject hitObject = hit.collider.gameObject;
+                JumpToSpark(hitObject.GetComponent<Spark>());
+            }
+        }
+
+        private void JumpToSpark(Spark spark)
+        {
+            if (spark && spark != m_Spark)
+            {
+
+            }
+        }
+    }
+}
--- Assets/Scripts/Wires/Wire.cs
+++ Assets/Scripts/Wires/Wire.cs
@@ -6,19 +6,30 @@ namespace Liminal.Wires
 {
     public class Wire : MonoBehaviour
     {
-        private Vector3 m_Start;        // Start of this wire
-        private Vector3 m_End;          // End of this wire
-        private Spark m_Spark;          // Spark attached to this wire
+        private Vector3 m_Start;                // Start of this wire
+        public float m_Distance = 15f;         // The distance of this wire
+
+        private Spark m_Spark;
+
+        void Awake()
+        {
+            m_Start = transform.position;
+        }
 
         void Start()
         {
            
         }
 
-        // Update is called once per frame
         void Update()
         {
 
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + (WireManager.WirePlane * m_Distance));
         }
     }
 }
