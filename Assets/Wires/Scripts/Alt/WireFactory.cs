4aeee4808558cc78eb2eb2a8526b514d06d67f3c
MODriscoll <MODriscoll55@outlook.com>
2019-06-25 21:40:23 +1000

    Researched and started to implement wire factories

--- Assets/Wires/Scripts/Alt/WireFactory.cs
+++ Assets/Wires/Scripts/Alt/WireFactory.cs
@@ -8,8 +8,13 @@ namespace TO5.Wires
     /// Factory for the types of wires to spawn into the world
     /// </summary>
     [CreateAssetMenu]
-    public class WireFactory : MonoBehaviour
+    public class WireFactory : ScriptableObject
     {
+        // The color for this factory
+        public Color color { get { return m_Color; } }
+
+        [SerializeField] private Color m_Color;     // Wires color
+
         private MaterialPropertyBlock m_WireMaterialProperties;
     }
 }
